bdafc08aef9f2dc3021225e191869c8fbd8836df
dsundarpaul <109967226+dsundarpaul@users.noreply.github.com>
2024-01-24 15:39:41 +0530

    added background grandient bolbs and added new views

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,26 +3,52 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/nextjs";
 import AppLayout from "~/layouts/AppLayout";
 import Script from "next/script";
+import { type ReactNode } from "react";
+import GuestLayout from "~/layouts/GuestLayout";
+// import GuestLayout from "~/layouts/GuestLayout";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  const renderCoreComponent = () => (
+    <>
+      <script
+        type="module"
+        defer
+        src="https://cdn.jsdelivr.net/npm/ldrs/dist/auto/helix.js"
+      ></script>
+      <Component {...pageProps} />
+    </>
+  );
+
   return (
     <>
       <ClerkProvider {...pageProps}>
-        <AppLayout>
-          <script
-            type="module"
-            defer
-            src="https://cdn.jsdelivr.net/npm/ldrs/dist/auto/helix.js"
-          ></script>
-          <Component {...pageProps} />
-        </AppLayout>
+        {/* <AppLayout>{renderCoreComponent()}</AppLayout> */}
+
+        <SignedInView>{renderCoreComponent()}</SignedInView>
+        <SignedOutView>{renderCoreComponent()}</SignedOutView>
       </ClerkProvider>
       <Script src="https://cdn.jsdelivr.net/npm/prismjs@1/components/prism-core.min.js" />
       <Script src="https://cdn.jsdelivr.net/npm/prismjs@1/plugins/autoloader/prism-autoloader.min.js" />
     </>
+  );
+};
+
+const SignedInView = ({ children }: { children: ReactNode }) => {
+  return (
+    <SignedIn>
+      <AppLayout>{children}</AppLayout>
+    </SignedIn>
+  );
+};
+
+const SignedOutView = ({ children }: { children: ReactNode }) => {
+  return (
+    <SignedOut>
+      <GuestLayout>{children}</GuestLayout>
+    </SignedOut>
   );
 };
 
